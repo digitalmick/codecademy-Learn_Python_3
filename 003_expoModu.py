b42874f1d8be39e0974e1369e1663ac298430060
digitalmick <digitalmick@gmail.com>
2026-07-14 15:15:24 -0400

    new files

---
+++ 003_expoModu.py
@@ -0,0 +1,45 @@
+# Exponents: the power of a number
+
+# 2 to the 10th power, or 1024
+print(2 ** 10)
+
+# 8 squared, or 64
+print(8 ** 2)
+
+# 9 * 9 * 9, 9 cubed, or 729
+print(9 **3)
+
+# We can even perform fractional exponents
+# 4 to the half power, or 2
+print(4 ** 0.5)
+
+# ------------------------------------------------------------
+
+# Calculation of squares for:
+# 6x6 quilt
+print(6 ** 2)
+# 7x7 quilt
+print (7 ** 2)
+# 8x8 quilt
+print(8 ** 2)
+# How many squares for 6 people to have 6 quilts each that are 6x6?
+print((6 ** 2) * 36)
+
+# Modulo: gives the remainder of a division calculation
+
+# Prints 4 because 29 / 5 is 5 with a remainder of 4
+print(29 % 5)
+ 
+# Prints 2 because 32 / 3 is 10 with a remainder of 2
+print(32 % 3)
+ 
+# Modulo by 2 returns 0 for even numbers and 1 for odd numbers
+# Prints 0
+print(44 % 2)
+
+print(3 % 3) # Prints 0
+print(4 % 3) # Prints 1
+print(5 % 3) # Prints 2
+print(6 % 3) # Prints 0
+print(7 % 3) # Prints 1
+
