296bee4089a4de5ce6fc4a4bb07803bc12fbadd6
digitalmick <digitalmick@gmail.com>
2026-07-14 12:59:08 -0400

    adding files

---
+++ 001_intro.py
@@ -0,0 +1,22 @@
+# Print your first output to the screen
+print("Hello World!")
+
+# Variable assignment w/string + variable output; change the variable
+meal = "Pasta"
+print("Dinner: " + meal)
+meal = "Cheesecake"
+print("Dessert: " + meal)
+
+# Arithmetic operations
+result = 10 + 30
+print(result)
+result = 40 - 10
+print(result)
+result = 50 * 5
+print(result)
+result = 16 / 4
+print(result)
+result = 25 % 2
+print(result)
+result = 5 ** 3
+
---
+++ 002_varNum.py
@@ -0,0 +1,18 @@
+# Set the variable(s)
+
+coffee_price = 1.50
+number_of_coffee = 4
+
+# Print output the calculation(s)
+
+print(coffee_price * number_of_coffee)
+print(coffee_price)
+print(number_of_coffee)
+
+# Update the proce
+
+coffee_price = 2.00
+
+print(coffee_price * number_of_coffee)
+print(coffee_price)
+print(number_of_coffee)
