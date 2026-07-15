8694d6d78e11b55ee5959859fb18eaf15b629da4
digitalmick <digitalmick@gmail.com>
2026-07-15 16:08:26 -0400

    new files

---
+++ 0008_input.py
@@ -0,0 +1,5 @@
+# capturing input from the user to be stored in a variable
+
+inputUser = input("What is your favorite color?")
+
+print(inputUser)
---
+++ 006_tripleQuotes.py
@@ -0,0 +1,11 @@
+# Triple quotes ("""or ''') string doesn't end until the next set of triple quotes
+
+# Assign the string here
+to_you = """ 
+Stranger, if you passing meet me and desire to speak to me, 
+  why should you not speak to me?
+And why should I not speak to you?
+"""
+
+# print(to_you)
+print(to_you)
