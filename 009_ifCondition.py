92e1f3c14b3778304420e8d7513ee1d5806f701e
digitalmick <digitalmick@gmail.com>
2026-07-16 13:16:26 -0400

    new files

---
+++ 009_ifCondition.py
@@ -0,0 +1,31 @@
+# if condition, colon (:) acts as the "then" 
+
+if 2 == 4 - 2: 
+  print("apple")
+
+# Example 1  -----------------------------------------------------------
+
+# Enter a user name here, make sure to make it a string
+user_name = "angela_catlady_87"
+
+if user_name == "angela_catlady_87":
+  print("Get off my computer Dave!")
+
+# Example 2  -----------------------------------------------------------
+
+x = 20
+y = 20
+
+# Write the first if statement here:
+if x == y:
+  print("These numbers are the same")
+
+
+credits = 120
+
+# Write the second if statement here:
+if credits >= 120:
+  print("You have enough credits to graduate!")
+
+# Example 3 -----------------------------------------------------------
+
