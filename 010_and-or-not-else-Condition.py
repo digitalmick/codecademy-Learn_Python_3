c8bba857d8e7b717c30e9b3f38dd93bbd7e4dd41
digitalmick <digitalmick@gmail.com>
2026-07-17 08:04:12 -0400

    new files

--- 010_and-or-not-else-Condition.py
+++ 010_and-or-not-else-Condition.py
@@ -37,3 +37,46 @@ gpa = 2.0
 if credits >=120 or gpa >=2.0:
   print("You have met at least one of the requirements.")
 
+# Examples (NOT)
+
+not 1 + 1 == 2  # False
+not 7 < 0       # True
+
+# Example #1 ----------------------------------------------------------
+
+credits = 120
+gpa = 1.8
+
+if not credits >= 120:
+  print("You do not have enough credits to graduate.")
+
+if not gpa >= 2.0:
+  print("Your GPA is not high enough to graduate.")
+
+if not credits >= 120 and not gpa >= 2.0:
+  print("You do not meet either requirement to graduate!")
+
+# Example (ELSE)
+
+credits = 120
+gpa = 1.9
+
+if (credits >= 120) and (gpa >= 2.0):
+  print("You meet the requirements to graduate!")
+else:
+  print("You do not meet the requirements to graduate.")
+
+  # Example (ELIF)
+
+  grade = 86
+
+if grade >= 90:
+  print("A")
+elif grade >= 80:
+  print("B")
+elif grade >= 70:
+  print("C")
+elif grade >= 60:
+  print("D")
+else:
+  print("F")
