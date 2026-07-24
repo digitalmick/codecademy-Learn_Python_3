539d845a64d557462f9e3e90c48b8fef105d24fd
digitalmick <digitalmick@gmail.com>
2026-07-24 08:12:13 -0400

    new files

---
+++ 013_lists.py
@@ -0,0 +1,20 @@
+# List 
+list = ["tom", "bob", "lucy", "mary"]
+
+# List elements start at 0 forward and -1 backward
+print(list[3])
+
+list.append("larry")
+
+print(list)
+
+# Modify List Elements
+
+garden_waitlist = ["Jiho", "Adam", "Sonny", "Alisha"]
+
+garden_waitlist[1] = "Calla"
+garden_waitlist[-1] = "Alex"
+
+print(garden_waitlist)
+
+# 
