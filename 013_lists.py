faf0f720d0d4924d40873cae826e8185d1503b05
digitalmick <digitalmick@gmail.com>
2026-07-27 14:36:01 -0400

    new files

--- 013_lists.py
+++ 013_lists.py
@@ -47,3 +47,31 @@ print(ellies_score)
 
 # Modifying 2D Lists
 
+incoming_class = [["Kenny", "American", 9], ["Tanya", "Ukrainian", 9], ["Madison", "Indian", 7]]
+print(incoming_class)
+
+incoming_class[2][2] = 8
+print(incoming_class)
+
+incoming_class[-3][-3] = "Ken"
+print(incoming_class)
+
+# Review
+
+first_names = ["Ainsley", "Ben", "Chani", "Depak"]
+preferred_size = ["Small", "Large", "Medium"]
+
+preferred_size.append("Medium")
+print(preferred_size)
+
+customer_data = [["Ainsley","Small",True], ["Ben","Large",False], ["Chani","Medium",True], ["Depak","Medium",False]]
+print(customer_data)
+
+customer_data[2][2] = False
+print(customer_data)
+
+customer_data[1].remove(False)
+print(customer_data)
+
+customer_data_final = customer_data + [["Amit","Large",True], ["Karim","X-Large",False]]
+print(customer_data_final)
---
+++ 015_workingLists.py
@@ -0,0 +1,10 @@
+# Python List Methods
+# .count() = list method to count the number of occurrences of an element in a list
+# .insert() = list method to insert an element into a specific index of the list
+# .pop() = list method to remove an element from a specific index or from the end of the list
+# range() = built-in Python function to create a sequence of integers
+# len() = built-in Python function to get the length of a list
+# .sort() / .sorted() = method and a built-in function to sort a list
+# Exmaple syntax for methods: list.method(input)
+# Example syntax for a built-in function: builtinfunction(input)
+
