4855fea89e62c6af8498fb7e88c380f8ba0768d9
digitalmick <digitalmick@gmail.com>
2026-07-30 11:51:04 -0400

    new files

--- 016_lensSlice.py
+++ 016_lensSlice.py
@@ -1,3 +1,8 @@
+"""
+Your comments here
+"""
+
+
 # Your code below:
 toppings = ["pepperoni","pineapple","cheese","sausage","olives","anchovies","mushrooms"]
 prices = [2,6,1,3,2,7,2]
---
+++ 017_forLoops.py
@@ -0,0 +1,21 @@
+# In a for loop, we will know in advance how many times the loop will need to iterate because we will be working on a collection with a predefined length.
+
+board_games = ["Settlers of Catan", "Carcassone", "Power Grid", "Agricola", "Scrabble"]
+
+sport_games = ["football", "hockey", "baseball", "cricket"]
+
+for game in board_games:
+  print(game)
+
+for sport in sport_games:
+  print(sport)
+
+# for loop: using range()
+
+promise = "I will finish the python loops module!"
+
+for temp in range(5):
+  print(promise)
+
+# for loop: using while
+
